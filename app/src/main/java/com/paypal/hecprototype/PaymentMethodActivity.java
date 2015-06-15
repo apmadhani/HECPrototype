6c2cb7418acf9b650c6a6284181c71f83d13a534
Akshay Madhani <amadhani@paypal.com>
2015-06-15 15:05:30 -0700

    Happy Path for getting the card token

--- app/src/main/java/com/paypal/hecprototype/PaymentMethodActivity.java
+++ app/src/main/java/com/paypal/hecprototype/PaymentMethodActivity.java
@@ -2,50 +2,33 @@ package com.paypal.hecprototype;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
 
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class PaymentMethodActivity extends Activity {
 //    Bitmap paypal;
     private String username;
+    public static final String CART_TOKEN = "com.paypal.hecprototype.cartToken";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_method);
         username = getIntent().getStringExtra(LoginActivity.USERNAME);
-//        ImageView paypalbutton = (ImageView) findViewById(R.id.PayPalButton);
-//        paypal = bitmapFromURL(getString(R.string.paypalurl));
-//        paypalbutton.setImageBitmap(paypal);
     }
-
-//    public Bitmap bitmapFromURL(String src) {
-//        try {
-//            URL url = new URL(src);
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//            connection.setDoInput(true);
-//            connection.connect();
-//            InputStream input = connection.getInputStream();
-//            Bitmap myBitmap = BitmapFactory.decodeStream(input);
-//            return myBitmap;
-//        } catch (Exception e) {
-//            e.printStackTrace();;
-//            return null;
-//        }
-//    }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -65,7 +48,55 @@ public class PaymentMethodActivity extends Activity {
     public void sendNotificationPage(View view) {
         Intent intent = new Intent(this, SendNotificationActivity.class);
         intent.putExtra(LoginActivity.USERNAME, username);
+        String cartToken = createCart();
+        intent.putExtra(CART_TOKEN, cartToken);
         startActivity(intent);
+    }
+
+    private String createCart() {
+        final String[] response = new String[1];
+
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                HttpClient client = new DefaultHttpClient();
+                HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+                HttpResponse httpresponse;
+                JSONObject json = new JSONObject();
+                JSONObject cartObj = new JSONObject();
+
+                try {
+                    HttpPost post = new HttpPost("http://10.0.2.2:80/api/cart/submit");
+                    JSONArray cart = new JSONArray(new String[]{"standard"});
+                    cartObj.put("cart", cart);
+                    json.put("clientID", 1);
+                    json.put("clientKey", "cb91d8ecbe332c731db35e5bd018dacd39359a0d4bde5f5d33b6b438a19df9fa");
+                    json.put("data", cartObj);
+                    StringEntity se = new StringEntity(json.toString());
+                    se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                    post.setEntity(se);
+                    httpresponse = client.execute(post);
+
+            /*Checking response */
+                    if(httpresponse!=null){
+                        //Get the data in the entity
+                        JSONObject jsonResponse = new JSONObject(EntityUtils.toString(httpresponse.getEntity()));
+                        response[0] = (String) ((JSONObject) jsonResponse.get("data")).get("token");
+                    }
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    response[0] = e.getMessage();
+                }
+            }
+        });
+
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return response[0];
     }
 
     public void doNothing(View view) {
